50086a549d1ed44427b42c1ffcc282cb064995e5
Pablo <pablo@pablo>
2021-12-09 23:21:44 +0100

    add day1

---
+++ src/day1.rs
@@ -0,0 +1,31 @@
+use crate::inputs;
+
+#[allow(dead_code)]
+pub fn part1() {
+    let lines = inputs::read_lines("./src/inputs/day1.txt");
+    let mut sum: u32 = 0;
+    lines.windows(2)
+    .inspect(|w| {
+        if w[0].parse::<i32>().unwrap() < w[1].parse::<i32>().unwrap() {
+            sum += 1;
+        }
+    }).for_each(drop);
+    println!("{}", sum);
+
+}
+
+#[allow(dead_code)]
+pub fn part2() {
+    let lines = inputs::read_lines("./src/inputs/day1.txt");
+    let mut sum: u32 = 0;
+    lines.iter()
+    .map(|w| w.parse::<i16>().unwrap())
+    .collect::<Vec<i16>>()
+    .windows(4)
+    .inspect(|w| {
+        if w[..3].iter().sum::<i16>() < w[1..].iter().sum::<i16>() {
+            sum += 1;
+        }
+    }).for_each(drop);
+    println!("{}", sum);
+}
