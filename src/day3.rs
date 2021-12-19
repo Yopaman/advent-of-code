b065d413539daccad3bd6355d4affeefd8b029f7
Yopaman <yopaplayer@gmail.com>
2021-12-19 20:11:14 +0100

    day 3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -1,7 +1,6 @@
 use crate::inputs;
 
-
-
+#[allow(dead_code)]
 pub fn part1() {
     let lines = inputs::read_lines("./src/inputs/day3.txt");
     let mut ones: [u32;12] = [0,0,0,0,0,0,0,0,0,0,0,0];
@@ -31,4 +30,41 @@ pub fn part1() {
 
     println!("{:?}", gamma_dec*eps_dec);
 
+}
+
+fn most_common_bit(lines: &Vec<String>, i: usize) -> u32 {
+    let nbr_zeros = lines.iter().fold(0, |acc, l| if l.chars().nth(i).unwrap() == '0' {acc + 1} else {acc});
+    let nbr_ones = lines.iter().fold(0, |acc, l| if l.chars().nth(i).unwrap() == '1' {acc + 1} else {acc});
+    if nbr_ones >= nbr_zeros {1} else {0}
+}
+
+pub fn part2() {
+    let lines = inputs::read_lines("./src/inputs/day3.txt");
+    
+
+    let mut o2_rating: Vec<String> = lines.clone();
+    let mut i = 0;
+    let mut mcb_o2: u32;
+    while o2_rating.len() > 1 {
+        mcb_o2 = most_common_bit(&o2_rating, i);
+        o2_rating = o2_rating.into_iter().filter(|l| mcb_o2 == l.chars().nth(i).unwrap().to_string().parse::<u32>().unwrap()).collect();
+        i += 1;
+    }
+    let o2_rating_dec = isize::from_str_radix(&o2_rating[0][..], 2).unwrap();
+    println!("o2 rating : {}", o2_rating_dec);
+
+    let mut co2_rating: Vec<String> = lines.clone();
+    let mut j = 0;
+    let mut mcb_co2: u32;
+    while co2_rating.len() > 1 {
+        mcb_co2 = most_common_bit(&co2_rating, j);
+        co2_rating = co2_rating.into_iter().filter(|l| { 
+            mcb_co2 != l.chars().nth(j).unwrap().to_string().parse::<u32>().unwrap()
+        }).collect();
+        j += 1;
+    }
+    let co2_rating_dec = isize::from_str_radix(&co2_rating[0][..], 2).unwrap();
+    println!("co2 rating : {}", co2_rating_dec);
+
+    println!("Solution: {}", co2_rating_dec * o2_rating_dec);
 }
