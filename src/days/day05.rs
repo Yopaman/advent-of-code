2e331f27b8decd7eb5c5030a9e696e4cb27b6dc2
Yopaman <pablohardouin02@gmail.com>
2023-12-07 12:55:00 +0100

    add day5 part1 & day6

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -1,13 +1,140 @@
 use crate::problem::Problem;
+use std::ops::Range;
 
 pub struct DayFive {}
 
+#[derive(Debug)]
+struct Converter {
+    source_ranges: Vec<Range<usize>>,
+    dest_ranges: Vec<Range<usize>>,
+}
+
+impl Converter {
+    fn convert(&self, src: &usize) -> usize {
+        for (i, sources) in self.source_ranges.iter().enumerate() {
+            if sources.contains(src) {
+                let range_index = src - sources.start;
+                return self.dest_ranges.get(i).unwrap().start + range_index;
+            }
+        }
+        *src
+    }
+
+    fn convert_range(&self, start: usize, end: usize) -> Vec<(usize, usize)> {
+        let mut res: Vec<(usize, usize)> = vec![];
+        for (i, sources) in self.source_ranges.iter().enumerate() {
+            match sources {
+                //  { [ } ]
+                s if start < s.start && end <= s.end => {
+                    let range_index = start - sources.start;
+                    res.push((start, s.start - 1));
+                    res.push((
+                        self.dest_ranges.get(i).unwrap().start,
+                        self.dest_ranges.get(i).unwrap().start + range_index,
+                    ));
+                }
+                // [ { ] }
+                s if start >= s.start && end > s.end => {
+                    let range_index = start - sources.start;
+                    res.push((
+                        self.dest_ranges.get(i).unwrap().start + range_index,
+                        self.dest_ranges.get(i).unwrap().end,
+                    ));
+                    res.push((s.end + 1, end));
+                }
+                // [ { } ]
+                s if start >= s.start && end <= s.end => {
+                    let range_index1 = start - s.start;
+                    let range_index2 = end - s.start;
+                    res.push((
+                        self.dest_ranges.get(i).unwrap().start + range_index1,
+                        self.dest_ranges.get(i).unwrap().start + range_index2,
+                    ));
+                }
+                // { [ ] }
+                s if start < s.start && end > s.end => {
+                    res.push((start, s.start - 1));
+                    res.push((s.end + 1, end));
+                    res.push((
+                        self.dest_ranges.get(i).unwrap().start,
+                        self.dest_ranges.get(i).unwrap().end,
+                    ));
+                }
+                _ => {
+                    res.push((start, end));
+                }
+            };
+        }
+        res
+    }
+}
+
+fn parse(input: String) -> (Vec<usize>, Vec<Converter>) {
+    // Parse seeds list
+    let mut blocks = input.split("\n\n");
+    let tmp1_seeds = blocks.next().unwrap().to_string();
+    let tmp2_seeds = tmp1_seeds.strip_prefix("seeds: ").unwrap();
+    let seeds = tmp2_seeds
+        .split_whitespace()
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect::<Vec<usize>>();
+
+    // Parse each converters
+    let mut converters: Vec<Converter> = vec![];
+    for block in blocks {
+        let mut lines = block.trim().split('\n');
+        lines.next();
+        let mut source_ranges = vec![];
+        let mut dest_ranges = vec![];
+        for line in lines {
+            let mut values = line.split_whitespace().map(|n| n.parse::<usize>().unwrap());
+            let dest = values.next().unwrap();
+            let source = values.next().unwrap();
+            let length = values.next().unwrap();
+            source_ranges.push(source..source + length + 1);
+            dest_ranges.push(dest..dest + length + 1);
+        }
+        converters.push(Converter {
+            source_ranges,
+            dest_ranges,
+        })
+    }
+    (seeds, converters)
+}
+
 impl Problem for DayFive {
     fn part_one(&self, input: String) {
-        todo!();
+        let (mut seeds, converters) = parse(input);
+        for converter in converters {
+            for seed in &mut seeds {
+                *seed = converter.convert(seed);
+            }
+        }
+        println!("{:?}", seeds.iter().min().unwrap());
     }
 
     fn part_two(&self, input: String) {
-        todo!();
+        let (seeds, converters) = parse(input);
+        let mut seeds_range = seeds
+            .chunks(2)
+            .map(|chunk| (chunk[0], chunk[0] + chunk[1]))
+            .collect::<Vec<(usize, usize)>>();
+        for converter in converters {
+            let mut res: Vec<(usize, usize)> = vec![];
+            for range in seeds_range {
+                let (start, end) = range;
+                let mut new_result = converter.convert_range(start, end);
+                res.append(&mut new_result);
+            }
+            seeds_range = res;
+        }
+        println!("{:?}", seeds_range);
+        let mut min = 9999999;
+        for (a, _) in seeds_range {
+            if a < min {
+                min = a;
+            }
+        }
+        println!("{:?}", min);
     }
 }
--- src/days/day06.rs
+++ src/days/day06.rs
@@ -2,12 +2,50 @@ use crate::problem::Problem;
 
 pub struct DaySix {}
 
+fn get_number_of_ways(time: usize, record: usize) -> usize {
+    let mut sum = 0;
+    for i in 0..time {
+        if i * (time - i) > record {
+            sum += 1;
+        }
+    }
+    sum
+}
+
 impl Problem for DaySix {
     fn part_one(&self, input: String) {
-        todo!();
+        let mut lines = input.split('\n');
+        let first_split = lines.next().unwrap().chars().skip(6).collect::<String>();
+        let times = first_split
+            .split_whitespace()
+            .map(|c| c.parse::<usize>().unwrap())
+            .collect::<Vec<usize>>();
+        let second_split = lines.next().unwrap().chars().skip(10).collect::<String>();
+        let records = second_split
+            .split_whitespace()
+            .map(|c| c.parse::<usize>().unwrap())
+            .collect::<Vec<usize>>();
+        let mut product = 0;
+        for (i, time) in times.iter().enumerate() {
+            let record = &records.get(i).unwrap();
+            let number = get_number_of_ways(*time, **record);
+            if product == 0 {
+                product = number;
+            } else {
+                product *= number;
+            }
+        }
+        println!("{}", product)
     }
 
     fn part_two(&self, input: String) {
-        todo!();
+        let mut lines = input.split('\n');
+        let mut time = lines.next().unwrap().chars().skip(6).collect::<String>();
+        time.retain(|c| !c.is_whitespace());
+        let time_nbr = time.parse::<usize>().unwrap();
+        let mut record = lines.next().unwrap().chars().skip(10).collect::<String>();
+        record.retain(|c| !c.is_whitespace());
+        let record_nbr = record.parse::<usize>().unwrap();
+        println!("{}", get_number_of_ways(time_nbr, record_nbr));
     }
 }
