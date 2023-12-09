bba8d8d588db3ed51430f9a5f3950ff2896df64a
Yopaman <pablohardouin02@gmail.com>
2023-12-09 22:56:56 +0100

    preparing for moving to one repo

--- src/inputs/mod.rs
+++
@@ -1,9 +0,0 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader};
-
-pub fn read_lines(file: &str) -> Vec<String> {
-    let f = File::open(file).expect("Unable to open file");
-    let f = BufReader::new(f);
-
-    f.lines().map(|l| l.expect("Could not parse line")).collect()
-}
